3a84e955416f3021a1105c1ea294fc65e3648a66
inasshh <inasaulia00@gmail.com>
2024-12-12 10:10:38 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,8 +4,31 @@ public class Nomor1 {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
+        //Meminta input dari user
+        System.out.print("Masukkan nilai N : ");
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+
+        //Menampilan deret FizzBuzz
+        System.out.println("Deret FizzBuzz dari 0 hingga " + N + ":");
+        tampilkanDeretFizzBuzz(N);
+    }
+
+    //Prosedur menampilkan FizzBuzz
+    public static void tampilkanDeretFizzBuzz(int N) {
+        for (int i = 0; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        
 
         /* Pekerjaan anda berakhir sini */
 
@@ -13,3 +36,4 @@ public class Nomor1 {
     }
     
 }
+
